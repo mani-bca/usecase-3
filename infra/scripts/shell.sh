1dfecbc4c5fe484c7720a5cb22e34bda789e6bdc
manivasagan <80743457+mani-bca@users.noreply.github.com>
2025-05-06 13:26:43 +0000

    albfg

--- infra/scripts/shell.sh
+++ infra/scripts/shell.sh
@@ -1,18 +1,7 @@
 #!/bin/bash
-# Update and install Docker
-apt-get update -y
-apt-get install -y docker.io
- 
-# Enable and start Docker service
-systemctl enable docker
-systemctl start docker
- 
- 
-docker run -it -p 8080:80 \
-  -e OPENPROJECT_SECRET_KEY_BASE=secret \
-  -e OPENPROJECT_HOST__NAME=0.0.0.0:8080 \
-  -e OPENPROJECT_HTTPS=false \
-  -e OPENPROJECT_DEFAULT__LANGUAGE=en \
-  openproject/openproject:15
- 
- 
+              #!/bin/bash
+              apt-get update -y
+              apt-get install -y docker.io
+              systemctl start docker
+              systemctl enable docker
+              docker run -dit -p 80:80 -e OPENPROJECT_SECRET_KEY_BASE=secret -e OPENPROJECT_HOST__NAME=0.0.0.0:80 -e OPENPROJECT_HTTPS=false openproject/community:12
